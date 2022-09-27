16c4050cc3fc850cb7f694c352ffa89036dafd32
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2022-09-27 17:52:03 +0200

    affichage des lettres trouvées, les non trouvées restent sous les '*'

--- app_pendu.java
+++ app_pendu.java
@@ -14,98 +14,84 @@ public class app_pendu {
         Random chf = new Random();
         int randomNumber = chf.nextInt(maxValueRandom) + minValueRandom;
 
-
-
-        // Todo ici prévoir la boucle et le nombre de chances
-
         int chances = 7;
+
+        ArrayList<Character> arrayStarsWord = new ArrayList<Character>(); // Array destiné à recevoir les * pour couvrir
+                                                                          // le mot
+
+        // while (chances > 0) {
+
+        // Todo décompter le nombre de chances
+
+        // Afficher le mot mystère
+        System.out.println(dicoWords[randomNumber]);
+        String theWord;
+        theWord = dicoWords[randomNumber];
+
+        // System.out.print(theWord.getClass());
+
+        // convertir le mot qui est en STR en arrayList
+
+        char[] ch = new char[theWord.length()];
+
+        ArrayList<Character> arrayWord = new ArrayList<Character>(); // passer le mot de STR à Array
+        for (int i = 0; i < theWord.length(); i++) {
+            ch[i] = theWord.charAt(i);
+            arrayWord.add(ch[i]);
+        }
+
+        // masquer le mot magique sous des étoiles
+
+        char[] chStars = new char[theWord.length()];
+        for (int i = 0; i < theWord.length(); i++) {
+            chStars[i] = '*';
+            arrayStarsWord.add(chStars[i]);
+        }
+        // afficher le mot magique sous des étoiles au format String
+        String starsword = "";
+
+        for (Character letter : arrayStarsWord) {
+            starsword += letter;
+
+        }
+        System.out.println("Découvre le mot mystère caché sous les étoiles : " + starsword);
+
+        ArrayList<Character> arrayUnStarsWord = new ArrayList<Character>(theWord.length());
+
+
+
         while (chances > 0) {
-            chances--;
-
-            // Todo le nombre de chances
-
-            // Afficher le mot mystère
-            System.out.println(dicoWords[randomNumber]);
-            String theWord;
-            theWord = dicoWords[randomNumber];
-
-            // System.out.print(theWord.getClass());
-
-            // convertir le mot qui est en STR en arrayList
-
-            char[] ch = new char[theWord.length()];
-            ArrayList<Character> arrayWord = new ArrayList<Character>();
-            for (int i = 0; i < theWord.length(); i++) {
-                ch[i] = theWord.charAt(i);
-
-                arrayWord.add(ch[i]);
-            }
-
-            // masquer le mot magique sous des étoiles
-
-            ArrayList<Character> arrayStarsWord = new ArrayList<Character>();
-
-            char[] chStars = new char[theWord.length()];
-            for (int i = 0; i < theWord.length(); i++) {
-                chStars[i] = '*';
-                arrayStarsWord.add(chStars[i]);
-            }
-            // afficher le mot magique sous des étoiles au format String
-            String starsword = "";
-
-            for (Character letter : arrayStarsWord) {
-                starsword += letter;
-
-            }
-            System.out.println(starsword);
 
             // récupérer la saisie utilisateur
 
             Scanner scannerLetter = new Scanner(System.in);
             System.out.println("Saisissez une lettre : ");
+
             char charUserInput = scannerLetter.next().charAt(0);
-            System.out.println("Vous avez saisi : " + charUserInput);
-            System.out.println(charUserInput);
 
             // Créer le démascage des caractères ex : **e***
-            // le ArrayList doit contenir autant d'indexe que le ArrayList qui contient les étoiles
-            ArrayList<Character> arrayUnStarsWord = new ArrayList<Character>();
-            for(int i= 0;i<arrayStarsWord.size(); i++){
-                arrayUnStarsWord.add('*');
-            }
+            // le ArrayList doit contenir autant d'indexe que le ArrayList qui contient les
+            // étoiles
 
-            // Todo vérifier si la saisie utilsateur comprend une correspondance ou
-            // plusieurs
-            // avec
-            // le mot magique
+            //ArrayList<Character> arrayUnStarsWord = new ArrayList<Character>(theWord.length());
+
+            //vérifier si la saisie utilsateur comprend une correspondance ou plusieurs avec le mot magique
+
             for (int i = 0; i < arrayWord.size(); i++) {
                 if (charUserInput == arrayWord.get(i)) {
-                    arrayUnStarsWord.set(i, charUserInput);
-
-                } else {
-                    arrayUnStarsWord.set(i, '*');
-                }
+                    arrayStarsWord.set(i, charUserInput);
+                } 
 
             }
-            System.out.println(arrayUnStarsWord);
-
-
+            System.out.println("Vos découvertes " + arrayStarsWord);
+            System.out.println("Il vous reste : " + chances);
         }
-
     }
-
 }
 
 /*
- * 3) instancier un nombre de chance max
- * 5) tant que l'utilisateur a des chances il peut recommancer
- * 6) si la lettre est bonne découvrir la lettre validée
- * 7) si la lettre est mauvaise décompter une chance
- * 8) si les chances sont épuisées alors terminer le jeu
- * 
- * public static String repettte(String starsword){
- * System.out.println("Le mot magique est ------------->" + starsword);
- * return starsword;
- * }
- * 
+ * convertire le arrayStarsWord en String et l'afficher en sortie  
+ * Prévoir une sortie de boucle si l'utilisateur trouve toutes les lettres avant la fin de ses chances
+ * ajouter un mécanisme de décompte des chances
+ * créer une boucle pour demander à l'utilisateur s'il veut recommancer
  */
