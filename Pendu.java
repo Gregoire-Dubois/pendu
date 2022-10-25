80ce33b76e246748ed497a9b62e2ad7edfdf60fb
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2022-10-25 13:02:46 +0200

    sauvegarde point de parcours

--- Pendu.java
+++ Pendu.java
@@ -2,10 +2,111 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+public class Pendu {
+
+    ArrayList<Character> ArrayWord = new ArrayList<Character>();
+    ArrayList <Character> anonymusWord = new ArrayList<Character>();
+
+
+    public void wordGenerator(){
+        // générator for create one word from dictionnaryWords
+        final String dictionnaryWords[] = {"java", "Python", "javascript", "cobol", "pearl", "c++","crêpes", "Bretagne"};
+         String theWord;
+
+        // générator for create one word from dictionnaryWords
+
+        //int min=0;
+        int max=dictionnaryWords.length;
+        Random nombre = new Random();
+        int nb;
+        nb = nombre.nextInt(max );
+        theWord = dictionnaryWords[nb];
+
+        // générator for create one word from dictionnaryWords
+
+        System.out.println(theWord);
+
+        // hide the word under stars
+        char anonymous = '*';
+        char[] letter = new char[theWord.length()];
+        String stars = " ";
+
+        for(int i =0; i< theWord.length(); i++)
+        {
+            letter[i] = theWord.charAt(i);
+            ArrayWord.add(letter[i]);
+        }
+
+        char[] starLetter = new char[theWord.length()];
+        for(int i =0; i < theWord.length(); i++)
+        {
+            starLetter[i] = anonymous;
+            anonymusWord.add(starLetter[i]);
+        }
+
+        for(Character star : anonymusWord)
+        {
+            stars += star+"";
+        }
+        System.out.println("_____________________________");
+        System.out.println("Le mot mystère est : " + stars);
+        System.out.println("_____________________________");
+
+        play(theWord,stars);
+    }
+
+    public void play(String theWord, String stars){
+
+        char attemptletter;
+        String discoveryLetters="";
+
+        int max = 9; // Max chances
+        int chances=0;
+
+        int reste = max - chances;
+
+        while ( chances < max){
+
+            // catch input's user
+            System.out.println("Entrez une lettre pour tenter de trouver le mot mystère");
+            Scanner userinp = new Scanner(System.in);
+            char attempt = userinp.next().charAt(0);
+            attemptletter = attempt;
+
+            for (int i = 0; i < ArrayWord.size(); i++) {
+                if (attemptletter == ArrayWord.get(i)) {
+                    anonymusWord.set(i, attemptletter);
+                }
+
+            }
+
+            discoveryLetters = anonymusWord.toString().replace(",", "");
+
+            if (anonymusWord.equals(ArrayWord)) {
+                System.out.println("Félicitation le mot était " + discoveryLetters);
+                break;
+            }
+
+            System.out.println(discoveryLetters);
+            System.out.println("Il vous reste encore " + reste + " chances");
+        }
+
+    }
+
+
+
+}
+
+
+
+/*import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Pendu
 {
 
-    private String dictionnaryWords[] = {"java", "Python", "javascript", "cobol", "pearl", "c++","crêpes", "kouign-amann"};
+    final String dictionnaryWords[] = {"java", "Python", "javascript", "cobol", "pearl", "c++","crêpes", "Bretagne"};
     private static String theWord;
 
     private static char attemptletter;
@@ -17,7 +118,10 @@ public class Pendu
 
     private static String discoveriesLetters="";
 
-    private final int chances =7;
+    public static boolean foundWord = false;
+
+    public String discoveryLetters;
+
 
     public Pendu()
     {
@@ -40,8 +144,6 @@ public class Pendu
 
         return theWord;
     }
-
-
 
     public String anonymousWord()
     {
@@ -82,19 +184,24 @@ public class Pendu
         return attemptletter = attempt;
     }
 
-
-    public void verification()
-    {
-        for(int i =0; i < ArrayWord.size(); i++)
-        {
-            if(attemptletter == ArrayWord.get(i)){
+    public String verification() {
+        for (int i = 0; i < ArrayWord.size(); i++) {
+            if (attemptletter == ArrayWord.get(i)) {
                 anonymusWord.set(i, attemptletter);
             }
-
         }
-        String discoveryLetters = anonymusWord.toString().replace(",","");
+
+        discoveryLetters = anonymusWord.toString().replace(",", "");
         System.out.println(discoveryLetters);
+
+        if (anonymusWord.equals(ArrayWord)) {
+            System.out.println("Félicitation le mot était " + discoveryLetters);
+        }
+        return  discoveryLetters;
+
+
     }
 
-
 }
+
+ */
--- app_pendu.java
+++ app_pendu.java
@@ -5,27 +5,9 @@ public class App_pendu {
 
     public static void main(String[] args) {
 
-        char selectPlayer =' ';
-
-        while(selectPlayer != 'N'){
-
-            Pendu jeu = new Pendu();
-            jeu.wordGenerator();
-            jeu.anonymousWord();
-
-            int retry=0;
-
-            while(retry<8) {
-
-                jeu.userInput();
-                jeu.verification();
-                retry++;
-            }
-
-            System.out.println("Souhaitez vous rejouer ? Tapez 'N' pour arreter ou 'Y' pour recommencer");
-            Scanner again = new Scanner(System.in);
-            selectPlayer = again.next().charAt(0);
-        }	
+        Pendu game = new Pendu();
+        game.wordGenerator();
+        	
     }
 }
 
